e94f8be06f8d76790f3f94b7ed7f63521ceb1b19
Pablo Zmdl <pablo@nextcloud.com>
2025-08-19 11:38:10 +0200

    A script to help publishing releases

---
+++ _bin/generate-downloads-json-data.php
@@ -0,0 +1,42 @@
+#!/usr/bin/env php
+<?php
+
+if (!isset($argv[1]) || !isset($argv[2])) {
+    echo 'Usage: ' . basename(__FILE__) . " BASEDIR_OF_TARBALLS VERSION\n";
+    exit(1);
+}
+
+if (!is_dir($argv[1])) {
+    echo "Error! Invalid directory: {$argv[1]}\n";
+    exit(1);
+}
+
+$basedir = $argv[1];
+$version = $argv[2];
+
+function human_file_size($byte)
+{
+    $factor = floor((strlen($byte) - 1) / 3);
+    if ($factor > 0) {
+        $sz = 'KMGT';
+    }
+    return sprintf('%.1f', $byte / 1024 ** $factor) . ' ' . @$sz[$factor - 1] . 'B';
+}
+
+function generate_data($version, $package_name, $filename)
+{
+    $sum = hash_file('sha256', $filename);
+    $size = human_file_size(filesize($filename));
+    return [
+        'package' => $package_name,
+        'url' => "https://github.com/roundcube/roundcubemail/releases/download/{$version}/{$filename}",
+        'size' => $size,
+        'checksum' => $sum,
+    ];
+}
+
+echo json_encode([
+    generate_data($version, 'Dependent', join(DIRECTORY_SEPARATOR, [$basedir, "roundcubemail-{$version}.tar.gz"])),
+    generate_data($version, 'Complete', join(DIRECTORY_SEPARATOR, [$basedir, "roundcubemail-{$version}-complete.tar.gz"])),
+    generate_data($version, 'Framework', join(DIRECTORY_SEPARATOR, [$basedir, "roundcube-framework-{$version}.tar.gz"])),
+], \JSON_PRETTY_PRINT | \JSON_UNESCAPED_SLASHES) . "\n";
